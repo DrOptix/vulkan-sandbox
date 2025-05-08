592ad08793eca2907602da9cd988830b75776246
Cristian Dinu <cristian.virgil.dinu@gmail.com>
2025-05-08 16:50:08 +0300

    feat: Create Vulkan instance
    We create a Vulkan instance, but for now we don't use it for anything.
    
    Because `ash::Instance` does not implement drop semantics we have to
    implement `Drop` for `HelloTriangleApplication` to destory the created
    Vulkan instance.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use anyhow::Result;
+use std::ffi::c_char;
+
+use anyhow::{Context, Result};
+use ash::vk;
 use glfw::{Glfw, GlfwReceiver};
 
 fn main() {
@@ -16,6 +19,8 @@ struct HelloTriangleApplication {
     glfw: Glfw,
     window: glfw::PWindow,
     window_events: GlfwReceiver<(f64, glfw::WindowEvent)>,
+    _entry: ash::Entry,
+    instance: ash::Instance,
 }
 
 /// Public functions
@@ -23,11 +28,19 @@ impl HelloTriangleApplication {
     pub fn new(width: u32, height: u32, title: &str) -> Result<Self> {
         let mut glfw = glfw::init_no_callbacks()?;
         let (window, window_events) = Self::create_window(&mut glfw, width, height, title)?;
+        let glfw_required_extensions = glfw
+            .get_required_instance_extensions()
+            .context("Vulkan is not supported")?;
+
+        let entry = unsafe { ash::Entry::load() }?;
+        let instance = Self::create_instance(&entry, &glfw_required_extensions)?;
 
         Ok(Self {
             glfw,
             window,
             window_events,
+            _entry: entry,
+            instance,
         })
     }
 
@@ -39,7 +52,8 @@ impl HelloTriangleApplication {
 
             for (_, event) in glfw::flush_messages(&self.window_events) {
                 println!("{:?}", event);
-                if let glfw::WindowEvent::Key(glfw::Key::Escape, _, glfw::Action::Press, _) = event {
+                if let glfw::WindowEvent::Key(glfw::Key::Escape, _, glfw::Action::Press, _) = event
+                {
                     self.window.set_should_close(true)
                 }
             }
@@ -65,5 +79,36 @@ impl HelloTriangleApplication {
         } else {
             anyhow::bail!("Unable to create GLFW window")
         }
+    }
+
+    fn create_instance(
+        entry: &ash::Entry,
+        required_extensions: &[String],
+    ) -> Result<ash::Instance> {
+        let app_info = vk::ApplicationInfo::default()
+            .api_version(vk::API_VERSION_1_3)
+            .engine_name(c"hello_trangle")
+            .engine_version(0)
+            .application_name(c"hello_trangle")
+            .engine_version(0);
+
+        let extension_pointers: Vec<*const c_char> = required_extensions
+            .iter()
+            .map(|ext| ext.as_ptr() as *const c_char)
+            .collect();
+
+        let create_info = vk::InstanceCreateInfo::default()
+            .enabled_extension_names(&extension_pointers)
+            .application_info(&app_info);
+
+        let instance = unsafe { entry.create_instance(&create_info, None)? };
+
+        Ok(instance)
+    }
+}
+
+impl Drop for HelloTriangleApplication {
+    fn drop(&mut self) {
+        unsafe { self.instance.destroy_instance(None) };
     }
 }
